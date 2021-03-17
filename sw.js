3739feb18bfd1139826d1d75438572e551a84561
henokali1 <henokali1@gmail.com>
2021-03-17 12:21:46 +0400

    first commit

---
+++ sw.js
@@ -0,0 +1,44 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/assets/js/ui.js',
+  '/assets/js/long-press-event.js',
+  '/assets/css/style.css',
+  '/assets/images/492x0w.png',
+  'https://fonts.googleapis.com/css?family=Lato:300,400,700',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://code.jquery.com/jquery-3.5.1.min.js',
+  '//db.onlinewebfonts.com/c/d04ad15ef66bc1f1295f87feee19dee2?family=ComoW01-Bold',
+  'https://code.jquery.com/jquery-3.5.1.min.js',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// When we change the name we could have multiple cache, to avoid that we need to delet the old cache, so with this function we check the key that is our cache naming, if it is different from the actual naming we delete it, in this way we will always have only the last updated cache.
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
